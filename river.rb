2f41729d9a7e6116f84309c8079c730764106293
audreybernstein <audrey.bernstein@aol.com>
2018-08-10 17:33:03 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,49 @@
 # river.rb
+class River
+  def initialize(name)
+    @name = name
+    @flood = false
+  end
+
+  def name 
+   @name
+  end 
+
+  def name=(river_name)
+   @name = river_name
+  end
+
+  def length
+   @length
+  end
+
+  def length=(river_length)
+    @length = river_length
+  end
+
+  def countries
+   @countries
+  end
+
+  def countries=(river_country)
+    @countries = river_country
+  end
+
+  def discharge 
+    @discharge
+  end
+  
+  def discharge=(discharge_river)
+    @discharge = discharge_river
+  end
+  
+  def flood
+    @flood
+  end
+  
+  def flood=(river_flood)
+    @flood = river_flood
+    river_flood = 1.3*discharge
+  end 
+end 
+
